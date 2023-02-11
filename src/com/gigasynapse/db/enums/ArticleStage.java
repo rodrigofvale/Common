eb62d1b8cd887f099c593cb921abbd794bbfd1bb
rodrigo <rodrigo@hplaptop>
2023-02-10 17:38:09 -0800

    Regular

--- src/com/gigasynapse/db/enums/ArticleStage.java
+++ src/com/gigasynapse/db/enums/ArticleStage.java
@@ -3,8 +3,7 @@ package com.gigasynapse.db.enums;
 public enum ArticleStage {
 	QUEUED(0), TOBEIGNORED(1), SAMECONTENT(2), READYFORBODY(3), READYTOINDEX(4), TBD1(5), TBD2(6), TBD3(7), SUMMARYFAILED(8), TOBEDELETED(9);
 	
-	private int status = 0;
-	
+	private int status = 0;	
 	public int getInt(){
 		return this.status;
 	}
